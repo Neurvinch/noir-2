1b1a503fbd81fae93cf8c0798776b03b5eba6d59
Naveen pandian <npandian515@gmail.com>
2025-05-08 14:01:37 +0530

    Refactor code structure for improved readability and maintainability

--- circuit/index.js
+++ circuit/index.js
@@ -3,39 +3,44 @@ import { Noir } from "@noir-lang/noir_js";
 import circuit from "./target/circuit.json"
 
 
-const show = (id, content) => {
-    const container = document.getElementById(id);
-    container.appendChild(document.createTextNode(content));
-    container.appendChild(document.createElement("br"));
-   };
-   
-   document.getElementById("submit").addEventListener("click", async () => {
-    try {
-     // noir goes here
-    } catch {
-     show("logs", "Oh 💔");
-    }
-   });
 
-    try {
+ 
 
-        const noir = new Noir(circuit)
-   const backend = new UltraHonkBackend(circuit.bytecode)
-   const age = document.getElementById("age").value;
-show("logs", "Generating witness... ⏳");
-const { witness } = await noir.execute({ age });
-show("logs", "Generated witness... ✅");
+    // try {
 
-
-show("logs", "Generating proof... ⏳");
-const proof = await backend.generateProof(witness);
-show("logs", "Generated proof... ✅");
-show("results", proof.proof);
+      
+    // } catch (error) {
         
-    } catch (error) {
-        
-    } 
+    // } 
   
+    const show = (id, content) => {
+        const container = document.getElementById(id);
+        container.appendChild(document.createTextNode(content));
+        container.appendChild(document.createElement("br"));
+       };
+       
+       document.getElementById("submit").addEventListener("click", async () => {
+        try {
+
+            const noir = new Noir(circuit)
+            const backend = new UltraHonkBackend(circuit.bytecode)
+            const age = document.getElementById("age").value;
+         show("logs", "Generating witness... ⏳");
+         const { witness } = await noir.execute({ age });
+         show("logs", "Generated witness... ✅");
+         
+         
+         show("logs", "Generating proof... ⏳");
+         const proof = await backend.generateProof(witness);
+         show("logs", "Generated proof... ✅");
+         show("results", proof.proof);
+                 
+         // noir goes here
+        } catch {
+         show("logs", "Oh 💔");
+        }
+       });
+       
 
    
 
--- circuit/vite.config.js
+++ circuit/vite.config.js
@@ -1,8 +1,17 @@
+import { defineConfig } from 'vite';
+import path from 'path';
 
-
-export default {
-    optimizeDeps: {
-      esbuildOptions: { target: "esnext" },
-      exclude: ['@noir-lang/noirc_abi', '@noir-lang/acvm_js']
+export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: { target: 'esnext' },
+    exclude: ['@noir-lang/noirc_abi', '@noir-lang/acvm_js']
+  },
+  server: {
+    fs: {
+      allow: [
+        '.', // allow current working directory
+        path.resolve(__dirname, 'node_modules/@noir-lang')
+      ]
     }
-  };
+  }
+});
