9498711afd92028d9f177e79a502346183342f1f
Naveen pandian <npandian515@gmail.com>
2025-05-08 14:11:16 +0530

    Update metadata and configuration to improve dependency management and exclude specific worker files

--- circuit/vite.config.js
+++ circuit/vite.config.js
@@ -4,12 +4,17 @@ import path from 'path';
 export default defineConfig({
   optimizeDeps: {
     esbuildOptions: { target: 'esnext' },
-    exclude: ['@noir-lang/noirc_abi', '@noir-lang/acvm_js']
+    exclude: [
+      '@noir-lang/noirc_abi',
+      '@noir-lang/acvm_js',
+      '@noir-lang/noir_js',         // <- very likely the source of `main.worker.js`
+      'main.worker.js'              // <- exclude the specific worker file
+    ]
   },
   server: {
     fs: {
       allow: [
-        '.', // allow current working directory
+        '.', 
         path.resolve(__dirname, 'node_modules/@noir-lang')
       ]
     }
